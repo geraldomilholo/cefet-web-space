3e41228b14f61a3cdcdccf45f1f4d3eda584c7c8
Geraldo <geraldomilholo@MacBook-Air-de-Geraldo.local>
2017-04-11 11:33:44 -0300

    cefet-web-space

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,20 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+(function expandCollapse() {
+	
+	let $expandButtons = document.querySelectorAll('button.botao-expandir-retrair');
+	for (var i = 0; i < $expandButtons.length; i++) {
+
+		$expandButtons[i].addEventListener('click', function(event) {
+			
+			let $parentElement = this.parentElement;
+			if ($parentElement.classList.contains('expandido')) {
+				$parentElement.classList.remove('expandido');
+			} else {
+				$parentElement.classList.add('expandido'); 
+			}
+
+		});
+	}
+	
+}());
--- exercicio2.js
+++ exercicio2.js
@@ -9,3 +9,43 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+var slider = function (indexImg) {
+
+	let $slide = document.querySelectorAll('#slide');
+	let srcImage = servidorDasImagens + todasAsImagens[indexImg];
+	$slide[0].src = srcImage;
+
+	//console.log("Image src = "  + srcImage);
+};
+
+(function next(){
+
+	let index = 0;
+	slider(index);
+
+	let $next = document.querySelectorAll('#proximo');
+	$next[0].addEventListener('click', function(event) {
+
+		let nextIndex = index + 1;
+		if (nextIndex > 4) {
+			nextIndex = 0;
+		} 
+
+		index = nextIndex;
+		slider(index);
+	});
+
+	let $before = document.querySelectorAll('#anterior');
+	$before[0].addEventListener('click', function(event) {
+
+		let beforeIndex = index - 1;
+		if (beforeIndex < 0) {
+			beforeIndex = 4;
+		} 
+
+		index = beforeIndex;
+		slider(index);
+	});
+
+}());
